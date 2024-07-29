a986818590e20891657eca5b9ed60b5d54665424
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-07-29 22:10:22 +0530

    till view.ejs

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', function (req, res) {
+  res.render("index.ejs");
+})
+
+app.listen(3000)
